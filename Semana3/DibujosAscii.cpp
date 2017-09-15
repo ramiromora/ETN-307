e0f1e4fae1b869cae6b8e4e1d2883ad001759bdd
Ramiro Mora <rvmora@gmail.com>
2017-09-15 16:28:56 -0400

    Update DibujosAscii.cpp

--- Semana3/DibujosAscii.cpp
+++ Semana3/DibujosAscii.cpp
@@ -1,38 +1,40 @@
 #include <iostream>
 using namespace std;
 
+//Funcion que imprime espacios
 void espacios(int e){
 	for(int i=1;i<=e;i++)
 		cout<<" ";
 }
 
+//Funcion que imprime *
 void asteriscos(int a){
 	for(int i=1;i<=a;i++)
 		cout<<"*";
 	cout<<endl;
 }
 
+//Funcion que grafica triangulos
 void triangulo(int b){
 	unsigned int esp, ast=1;
-		if(b%2) 
-			esp=(b-1)/2;
-		else{
-			esp=(b-2)/2+1;
-			ast=0;
-		}
-		while(ast<=b){
-			espacios(esp);
-			asteriscos(ast);
-			ast=ast+2;
-			esp--;
-		}
+	esp=b/2;
+	if(!(b%2))
+		ast=0; 							//En caso de que sea la base un numero par
+	while(ast<=b){
+		espacios(esp);
+		asteriscos(ast);
+		ast=ast+2;
+		esp--;
+	}
 }
 
+//Funcion que grafica rectangulos
 void rectangulo(int b, int a){
 	for(int i=1;i<=a;i++)
 		asteriscos(b);
 }
 
+//Funcion que gradica paralelogramos
 void paralelogramo(int b, int a){
 	unsigned esp=a;
 	while(a>0){
@@ -42,6 +44,7 @@ void paralelogramo(int b, int a){
 	}	
 }
 
+//Funcion principal
 int main(){
 	unsigned int x,b,a;
 	cin >> x;
