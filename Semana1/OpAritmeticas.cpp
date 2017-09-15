7406908417ae1f7eee45db93fc34dd36cf4a4091
ramiromora <rvmora@gmail.com>
2017-09-15 11:09:39 -0400

    Create OpAritmeticas.cpp

---
+++ Semana1/OpAritmeticas.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	int a;
+	int b;
+	cout << "Ingrese dos numeros :" << endl;
+	cin >> a >> b;
+	cout << "La suma de " << a << " y " << b << " es: " << a+b << endl;
+	cout << "La resta de " << a << " y " << b << " es: " << a-b << endl;
+	cout << "La multiplicacion de " << a << " y " << b << " es: " << a*b << endl;
+	cout << "La division de " << a << " y " << b << " es: " << a/b << endl;
+	cout << "El modulo de " << a << " y " << b << " es: " << a%b << endl;
+    return 0;
+}
