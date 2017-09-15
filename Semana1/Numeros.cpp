4725d701e83d62b1c7ddeebfa38efe0eea0dad11
ramiromora <rvmora@gmail.com>
2017-09-15 11:09:13 -0400

    Create Numeros.cpp

---
+++ Semana1/Numeros.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "1\n";
+	cout << "\t2\n";
+	cout << "\t\t3\n";
+	cout << "\t\t\t4\n";
+	cout << "\t\t5\n";
+	cout << "\t6\n";
+	cout << "7\n";
+
+	cin.get();
+    return 0;
+}
