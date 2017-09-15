a091e84608049610e2a6ea49b0f95301b1edb57d
ramiromora <rvmora@gmail.com>
2017-09-15 11:07:14 -0400

    Create HolaMundo.cpp

---
+++ Semana1/HolaMundo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "Hola Mundo!" << endl;
+
+	cin.get(); //Mantiene la consola abierta hasta que se presione ENTER
+	
+    return 0;
+}
