4d09acff268d3edcee433be8885ea312b5bf6ebc
Ramiro Mora <rvmora@gmail.com>
2017-09-28 09:45:40 -0400

    Create NumeroFeliz.cpp

---
+++ Semana5/NumeroFeliz.cpp
@@ -0,0 +1,30 @@
+//
+//Números Felices
+//http://www.titancod.net/problema/NUMFELIZ
+//Se aprecia un error en el enunciado, ya que 338 debería ser un número TRISTE
+//
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    long n,r,p=0;
+    cin >> n;
+    n=pow(static_cast<double>(n),2);
+    do{
+      p=0;
+      do{
+      r=n%10;
+      n=(n-r)/10;
+      p=p+pow(static_cast<double>(r),2);
+      }while(n!=0);
+    n=p;
+    }while(p>9);
+    if(p==1) cout << "Feliz";
+    else cout << "Triste";
+    cout << endl;
+    return 0;
+}
