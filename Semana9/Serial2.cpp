eebddbae5217d2b360f6b23c39947848381dbfef
Ramiro Mora <rvmora@gmail.com>
2017-10-25 21:13:07 -0400

    Create Serial2.cpp

---
+++ Semana9/Serial2.cpp
@@ -0,0 +1,25 @@
+
+int incomingByte = 0;    // variable to hold the analog value
+int ledPin = 13; 
+
+void setup() {
+  // open the serial port at 9600 bps:
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+// read the analog input on pin 0:
+  if (Serial.available() > 0) {
+    incomingByte-=48;
+    Serial.println(incomingByte);
+    if(incomingByte%2){
+      digitalWrite(ledPin, HIGH);  // Enciende el LED 
+      delay(1000);      // Pause de 1 segundo 
+      digitalWrite(ledPin, LOW);  // Apaga el LED 
+      delay(1000);
+    }
+  }
+  // delay 10 milliseconds before the next reading:
+  delay(10);
+}
